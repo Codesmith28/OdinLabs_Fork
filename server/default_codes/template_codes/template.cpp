b02f8fccc5611c57651e47e170d58aa8d5a9c473
Codesmith28 <sarthaksiddhpura@gmail.com>
2025-11-10 13:53:49 +0530

    add language specific driver codes and template codes for reference

---
+++ server/default_codes/template_codes/template.cpp
@@ -0,0 +1,36 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // -----------------------------
+// // Basic Linked List Node
+// // -----------------------------
+// struct ListNode {
+//         int val;
+//         ListNode *next;
+//         ListNode(int x) : val(x), next(nullptr) {
+//         }
+// };
+
+// // -----------------------------
+// // Basic Binary Tree Node
+// // -----------------------------
+// struct TreeNode {
+//         int val;
+//         TreeNode *left;
+//         TreeNode *right;
+//         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+//         }
+// };
+
+class Solution {
+public:
+    void solve( ) {
+        // Write your code here
+    }
+};
+
+// int main( ) {
+//         Solution sol = Solution();
+//         sol.solve();
+//         return 0;
+// }
---
+++ server/default_codes/template_codes/template.java
@@ -0,0 +1,35 @@
+
+// import java.util.*;
+
+// // -----------------------------
+// // Basic Linked List Node
+// // -----------------------------
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) { val = x; next = null; }
+// }
+
+// // -----------------------------
+// // Basic Binary Tree Node
+// // -----------------------------
+// class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode(int x) { val = x; left = right = null; }
+// }
+
+class Solution {
+    public void solve() {
+        // Write your code here
+        System.out.println("Hello, World!");
+    }
+}
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         sol.solve();
+//     }
+// }
